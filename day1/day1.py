6f7e646d48e97269686eafe595ef4cc74d8dce78
Reed Haffner <reedhaffner@pm.me>
2018-12-07 17:42:31 -0800

    day 1 pt 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,5 @@
+# Day 1 Part 1
+
 freq = 0
 
 with open("frequencies.txt", "r") as f:
@@ -5,3 +7,19 @@ with open("frequencies.txt", "r") as f:
         freq += int(line)
 
 print(freq)
+
+# Day 1 Part 2
+
+freq = 0
+
+freqs = set()
+
+while True:
+
+    with open("frequencies.txt", "r") as f:
+        for line in f.readlines():
+            freq += int(line)
+            if freq in freqs:
+                print(freq)
+                quit()
+            freqs.add(freq)
